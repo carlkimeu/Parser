fd2f527373124d1ac8b5e35ec00f312a7b0e356d
carlkimeu <91551167+carlkimeu@users.noreply.github.com>
2022-05-05 14:53:52 +0300

    Create lldpad.sh

---
+++ lldpad.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+touch verify.yml
+
+
+
+echo "---" >> verify.yml
+
+echo "- name: \"Check lldpad version\"" >> verify.yml
+
+echo "  hosts: all" >> verify.yml
+
+echo "  gather_facts: no" >> verify.yml
+
+echo "  tasks:" >> verify.yml
+
+echo "    - name: \"Check lldpad version\"" >> verify.yml
+
+echo "      command: lldptool -V" >> verify.yml
+
+echo "      register: lldpad_version" >> verify.yml
+
+echo "      changed_when: False" >> verify.yml
+
+echo "    - name: \"Start lldpad service\"" >> verify.yml
+
+echo "      service:" >> verify.yml
+
+echo "        name: lldpad" >> verify.yml
+
+echo "        state: started" >> verify.yml
+
+
+
+cat verify.yml
+
+
+
+hash_output=$(sha1sum verify.yml)
+
+
+
+echo $hash_output verify.yml
